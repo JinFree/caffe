b04839ca0132ea63ab086fa947ba6e106025d126
JinFree <ytrqwe12@gmail.com>
2019-10-27 21:05:07 +0900

    install for tx2

--- install_caffe_for_tx2.sh
+++ install_caffe_for_tx2.sh
@@ -10,12 +10,9 @@ sudo apt-get install liblapack-dev liblapack3 libopenblas-base libopenblas-dev -
 sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev libatlas-base-dev liblmdb-dev libblas-dev libatlas-base-dev libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler -y
 cp Makefile.config.tx2 Makefile.config
 echo "export PYTHONPATH=$(pwd)/python:\$PYTHONPATH" >> ~/.bashrc
-sudo apt install python-pip python3-pip python-matplotlib python3-matplotlib-y
+sudo apt install python-pip python3-pip python-matplotlib -y
 pip install cython
-pip3 install cython 
-sudo apt install python-numpy python3-numpy -y
-pip install sklearn
-pip3 install sklearn
+sudo apt install python-numpy python-skimage -y
 make all -j4
+make runtest -j4
 make pycaffe
-
